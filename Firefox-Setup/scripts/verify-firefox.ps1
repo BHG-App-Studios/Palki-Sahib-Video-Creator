2cd7205e7f6e6ebed282d3758b2ba5a1c148afcd
Gurpreet Singh <iemgurpreet525@gmail.com>
2026-07-29 21:20:22 +0530

    final production

--- Firefox-Setup/scripts/verify-firefox.ps1
+++ Firefox-Setup/scripts/verify-firefox.ps1
@@ -58,7 +58,28 @@ function Save-Screenshot {
 Write-Host "Opening YouTube in Firefox to verify Google sign-in..."
 Start-Process -FilePath $FirefoxExecutable -ArgumentList @('https://www.youtube.com/channel/UCYn6UEtQ771a_OWSiNBoG8w/live')
 
-Write-Host 'Waiting 30 seconds for YouTube to load...'
+Write-Host 'Waiting 5 seconds for YouTube to load...'
+Start-Sleep -Seconds 5
+
+$firefoxWindow = Get-Process -Name firefox -ErrorAction SilentlyContinue |
+    Where-Object { $_.MainWindowHandle -ne 0 } |
+    Sort-Object StartTime -Descending |
+    Select-Object -First 1
+
+if ($null -eq $firefoxWindow) {
+    throw 'Could not find a visible Firefox window to start YouTube playback.'
+}
+
+$windowShell = New-Object -ComObject WScript.Shell
+if (-not $windowShell.AppActivate($firefoxWindow.Id)) {
+    throw 'Could not activate the Firefox window to start YouTube playback.'
+}
+
+Start-Sleep -Milliseconds 500
+Write-Host 'Sending Space key to start YouTube playback...'
+[System.Windows.Forms.SendKeys]::SendWait(' ')
+
+Write-Host 'Waiting 30 seconds before continuing...'
 Start-Sleep -Seconds 30
 
 Save-Screenshot -Name '01-youtube-sign-in-check.jpg'
