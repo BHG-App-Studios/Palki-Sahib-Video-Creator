0c002ac469890e836a5649bc1b53526ed50467db
Gurpreet Singh <iemgurpreet525@gmail.com>
2026-07-31 00:52:40 +0530

    Update project

--- Scripts/01_download_stream.py
+++ Scripts/01_download_stream.py
@@ -38,7 +38,7 @@ def configure_utf8_console():
 
 def get_live_video_url():
     print("Using hardcoded YouTube URL for testing.")
-    return "https://www.youtube.com/watch?v=0goh2R83VBA"
+    return "https://www.youtube.com/watch?v=kTmZkXTMmmc"
 
 
 def get_video_stream_url(video_url):
